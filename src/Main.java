7807f63b5abe8727bed1371d828b8431bbb918b8
ExoHeart <69839114+ExoHeart@users.noreply.github.com>
2023-09-12 14:07:13 +0800

    finished quiz 2.1! :D

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        //calling methods
+       int resultOne = add(1,2);
+        System.out.println(resultOne);
+
+        String resultTwo = greeting("Mr. Ewing");
+        System.out.println(resultTwo);
+
+        int resultThree = add(1, 2, 3, 4);
+        System.out.println(resultThree);
+
+        printMe("Happy friday!");
+
+    }
+
+    //add
+    public static int add(int numOne, int numTwo)
+    {
+        return numOne + numTwo;
+    }
+    //greeting
+    public static String greeting(String name)
+    {
+        return "Bonjour, " + name + "!";
+    }
+
+    //add 4 numbers
+    public static int add(int numOne, int numTwo, int numThree, int numFour)
+    {
+        int a;
+        int b;
+        a = add(numOne,numTwo);
+        b = add(numThree,numFour);
+        return add(a,b);
+    }
+
+    //print
+    public static void printMe(String toBePrinted)
+    {
+        System.out.println(toBePrinted);
+    }
 }
+
